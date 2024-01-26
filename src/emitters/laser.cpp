e6dbf5e8639ba0d81e56fbec60f0ce57fd7e9451
grandch <charliegrand31@gmail.com>
2024-01-26 17:20:25 +0100

    finish laser

--- src/emitters/laser.cpp
+++ src/emitters/laser.cpp
@@ -1,9 +1,5 @@
 #include <mitsuba/core/properties.h>
-#include <mitsuba/core/warp.h>
 #include <mitsuba/render/emitter.h>
-#include <mitsuba/render/medium.h>
-#include <mitsuba/render/mesh.h>
-#include <mitsuba/render/shape.h>
 #include <mitsuba/render/texture.h>
 
 NAMESPACE_BEGIN(mitsuba)
@@ -11,26 +7,15 @@ NAMESPACE_BEGIN(mitsuba)
 template <typename Float, typename Spectrum>
 class Laser final : public Emitter<Float, Spectrum> {
 public:
-    MI_IMPORT_BASE(Emitter, m_flags, m_medium, m_to_world)
+    MI_IMPORT_BASE(Emitter, m_flags)
     MI_IMPORT_TYPES(Scene, Texture)
 
     Laser(const Properties &props) : Base(props) {    
         m_position = props.get<ScalarPoint3f>("position");
         m_radiance = props.texture_d65<Texture>("radiance", 1.f);
-
-        if (props.has_property("direction")) {
-            if (props.has_property("to_world"))
-                Throw("Only one of the parameters 'direction' and 'to_world' "
-                      "can be specified at the same time!'");
-
-            ScalarVector3f direction(dr::normalize(props.get<ScalarVector3f>("direction")));
-            auto [up, unused] = coordinate_system(direction);
-
-            m_to_world = ScalarTransform4f::look_at(0.0f, ScalarPoint3f(direction), up);
-            dr::make_opaque(m_to_world);
-        }
-
-        m_flags = EmitterFlags::Surface | EmitterFlags::DeltaDirection;
+        m_radius = props.get<float>("radius");
+        m_direction = dr::normalize(props.get<ScalarVector3f>("direction"));
+        m_flags = EmitterFlags::DeltaPosition | EmitterFlags::DeltaDirection;
         dr::set_attr(this, "flags", m_flags);
     }
 
@@ -39,21 +24,16 @@ public:
         callback->put_object("radiance", m_radiance.get(), +ParamFlags::Differentiable);
     }
 
-    // std::pair<Ray3f, Spectrum> sample_ray(Float /*time*/, Float /*wavelength_sample*/,
-    //                                       const Point2f &/*sample2*/,
-    //                                       const Point2f & /*sample3*/,
-    //                                       Mask /*active*/) const override {
-    //     // 1. Sample spatial component
-    //     // 2. Directional component is the normal vector at that position.
-    //     // 3. Sample spectral component
-    //     // SurfaceInteraction3f si(ps, dr::zeros<Wavelength>());
-    //     // auto [wavelength, wav_weight] = sample_wavelengths(si, wavelength_sample, active);
-    //     // si.time = time;
-    //     // si.wavelengths = wavelength;
-
-    //     // return { si.spawn_ray(d), m_area * wav_weight };
-    //     return { 0, 0 };
-    // }
+    /*
+    * Don't use this method
+    * The laser emitter is designed to be used with a VolPath Integrator and it doesn't make sense to implement the sample_ray method.
+    */
+    std::pair<Ray3f, Spectrum> sample_ray(Float /*time*/, Float /*wavelength_sample*/,
+                                          const Point2f &/*sample2*/,
+                                          const Point2f & /*sample3*/,
+                                          Mask /*active*/) const override {                                  
+        return { Ray3f(0,0), Spectrum(0) };
+    }
 
     /**
      * Current strategy: don't try to connect this emitter
@@ -66,25 +46,64 @@ public:
      *       flat surface.
      */
     std::pair<DirectionSample3f, Spectrum>
-    sample_direction(const Interaction3f & /*it*/, const Point2f & /*sample*/,
-                     Mask /*active*/) const override {
-        return { dr::zeros<DirectionSample3f>(), dr::zeros<Spectrum>() };
+    sample_direction(const Interaction3f & it, const Point2f & /*sample*/,
+                     Mask active) const override {
+        
+        Float a = dr::dot(-m_direction, -m_direction);
+        Float b = 2 * dr::dot(-m_direction, it.p - m_position);
+        Float c = dr::dot(it.p - m_position, it.p - m_position) - m_radius * m_radius;
+        Float d = b*b - 4*a*c;
+
+        DirectionSample3f ds;
+        ds.p      = m_position;
+        ds.n      = -m_direction;
+        ds.uv     = 0.f;
+        ds.time   = it.time;
+        ds.pdf    = 0.f;
+        ds.delta  = true;
+        ds.emitter = this;
+        ds.d      = -m_direction;
+        ds.dist   = dr::norm(ds.p - it.p);
+        
+        if(dr::all(d < 0))
+        {
+            return {ds, Spectrum(0)};
+        }
+            
+        
+        Float t1 = (-b + dr::sqrt(d)) / (2 * a);
+        Float t2 = (-b - dr::sqrt(d)) / (2 * a);
+        
+        if(dr::all(0 <= t1 || 0 <= t2))
+        {
+            SurfaceInteraction3f si = dr::zeros<SurfaceInteraction3f>();
+            si.wavelengths = it.wavelengths;
+            ds.pdf = 1.f;
+            return {ds, depolarizer<Spectrum>(m_radiance->eval(si, active))};
+        }
+        
+        return {ds, dr::zeros<Spectrum>() };
     }
 
-    Float pdf_direction(const Interaction3f & /*it*/,
+    Float pdf_direction(const Interaction3f & it,
                         const DirectionSample3f & /*ds*/,
                         Mask /*active*/) const override {
+        Float a = dr::dot(-m_direction, -m_direction);
+        Float b = 2 * dr::dot(-m_direction, it.p - m_position);
+        Float c = dr::dot(it.p - m_position, it.p - m_position) - m_radius * m_radius;
+        Float d = b*b - 4*a*c;
+        
+        if(dr::any(d < 0))
+            return 0.f;
+        
+        Float t1 = (-b + dr::sqrt(d)) / (2 * a);
+        Float t2 = (-b - dr::sqrt(d)) / (2 * a);
+        
+        if(dr::any(dr::all(0 <= t1 && t1 <= 1) || dr::all(0 <= t2 && t2 <= 1)))
+            return 1.f;
+
         return 0.f;
     }
-
-    // std::pair<PositionSample3f, Float>
-    // sample_position(Float /*time*/, const Point2f &/*sample*/,
-    //                 Mask /*active*/) const override {
-    //     // PositionSample3f ps = m_shape->sample_position(time, sample, active);
-    //     // Float weight        = dr::select(ps.pdf > 0.f, dr::rcp(ps.pdf), 0.f);
-    //     // return { ps, weight };
-    //     return { 0, 0 };
-    // }
 
     /**
      * This will always 'fail': since `si.wi` is given,
@@ -97,10 +116,13 @@ public:
             si, math::sample_shifted<Wavelength>(sample), active);
     }
 
-    Spectrum eval(const SurfaceInteraction3f & /*si*/,
-                  Mask /*active*/) const override {
-        return 0.f;
+    Spectrum eval(const SurfaceInteraction3f & si,
+                  Mask active) const override {
+        return depolarizer<Spectrum>(m_radiance->eval(si, active));
     }
+
+    /// This emitter does not occupy any particular region of space, return an invalid bounding box
+    ScalarBoundingBox3f bbox() const override { return ScalarBoundingBox3f(); }
 
     std::string to_string() const override {
         std::ostringstream oss;
@@ -113,8 +135,9 @@ public:
     MI_DECLARE_CLASS()
 private:
     ref<Texture> m_radiance;
-    field<Point3f> m_position;
-    Float m_area = 0.f;
+    Point3f m_position;
+    ScalarVector3f m_direction;
+    ScalarFloat m_radius;
 };
 
 MI_IMPLEMENT_CLASS_VARIANT(Laser, Emitter)
